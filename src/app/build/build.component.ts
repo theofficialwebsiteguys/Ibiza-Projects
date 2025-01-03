4d83b8c75471d3b0c073243aa831e387f9c7673d
Full Name <theofficialwebsiteguys@gmail.com>
2025-01-03 15:30:22 -0500

    before and after images

--- src/app/build/build.component.ts
+++ src/app/build/build.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { HeroComponent } from '../hero/hero.component';
 
 @Component({
   selector: 'app-build',
   standalone: true,
   imports: [CommonModule, HeroComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './build.component.html',
   styleUrl: './build.component.scss'
 })
--- src/app/hero/hero.component.ts
+++ src/app/hero/hero.component.ts
@@ -13,4 +13,32 @@ export class HeroComponent {
   @Input() isHomePage: boolean = true; // Default to home page
   @Input() subpageTitle: string = 'Subpage Title'; // Title for subpages
   @Input() subpageImage: string = '../../assets/subpage-default.jpg'; // Default subpage image
+
+  backgroundImages = [
+    'assets/Gazebo/gazebo-full.jpg',
+    'assets/Gilchrest/side.jpg',
+    'assets/Additions/addition-display.jpg',
+    'assets/Shed/IMG_0730.jpg'
+  ];
+
+  currentImage = this.backgroundImages[0]; // Default image
+  private imageInterval: any;
+
+  ngOnInit(): void {
+    this.startImageRotation();
+  }
+
+  ngOnDestroy(): void {
+    if (this.imageInterval) {
+      clearInterval(this.imageInterval);
+    }
+  }
+
+  startImageRotation(): void {
+    let index = 0;
+    this.imageInterval = setInterval(() => {
+      index = (index + 1) % this.backgroundImages.length; // Cycle through images
+      this.currentImage = this.backgroundImages[index];
+    }, 4000); 
+  }
 }
